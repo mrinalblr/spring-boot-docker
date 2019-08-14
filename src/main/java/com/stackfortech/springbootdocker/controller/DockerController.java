ac10fcb2b2ca61ad8e25c93641348bbc1dce9af5
Mrinal Deo <mrinal.deo2@gmail.com>
2019-08-14 16:03:22 +0530

    initialized the repository with spring application

---
+++ src/main/java/com/stackfortech/springbootdocker/controller/DockerController.java
@@ -0,0 +1,16 @@
+package com.stackfortech.springbootdocker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/docker")
+public class DockerController {
+
+    @GetMapping("/hello")
+    public String getStarted(){
+
+        return "Hello from endpoint exposed inside a docker container";
+    }
+}
