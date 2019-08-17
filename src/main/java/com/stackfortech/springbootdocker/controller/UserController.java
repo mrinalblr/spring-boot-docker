6dfea93f44b07188e7c562a28837183ded000721
Mrinal Deo <mrinal.deo2@gmail.com>
2019-08-17 22:45:07 +0530

    added gitignore

--- src/main/java/com/stackfortech/springbootdocker/controller/UserController.java
+++ src/main/java/com/stackfortech/springbootdocker/controller/UserController.java
@@ -2,14 +2,16 @@ package com.stackfortech.springbootdocker.controller;
 
 import com.stackfortech.springbootdocker.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.ws.Response;
 
-@RestController("/api/user")
+@RestController()
+@RequestMapping("/api")
 public class UserController {
 
-    @GetMapping
+    @GetMapping("/user")
     public User getUser(){
         return new User("stackfortech","Version Control | Github Tutorial | GIT | DevOps","DevOps");
     }
